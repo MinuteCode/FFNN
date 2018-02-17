d27fa283bbcdf79035db7b4a9e1eceaa4eb09c21
Benjamin <benjamin.ameur@gmail.com>
2018-02-17 23:01:06 +0100

    added another layer to the NN

--- ConsoleApplication2/ConsoleApplication2.h
+++ ConsoleApplication2/ConsoleApplication2.h
@@ -1,3 +1,4 @@
 #pragma once
 
 double feedNetwork(std::vector<int> &entry, std::vector<Neuron> &inputLayer, std::vector<Neuron> &hiddenLayer, std::vector<Neuron> &outputLayer);
+double feedNetwork2(std::vector<int> &entry, std::vector<Neuron> &inputLayer, std::vector<Neuron> &hiddenLayer2, std::vector<Neuron> &hiddenLayer, std::vector<Neuron> &outputLayer);
